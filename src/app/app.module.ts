53a27f8bb80bcf63a5e02053f961d5503cb026b5
lyson <1154109022@qq.com>
2017-10-30 10:39:53 +0800

    git init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './public/header/header.component';
+import { MenuComponent } from './public/menu/menu.component';
+import { SidebarComponent } from './public/sidebar/sidebar.component';
+import { FooterComponent } from './public/footer/footer.component';
+import { ContentComponent } from './public/content/content.component';
+import { StockManageComponent } from './stock/stock-manage/stock-manage.component';
+import { StarsComponent } from './public/stars/stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    SidebarComponent,
+    FooterComponent,
+    ContentComponent,
+    StockManageComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/stock/stock-manage/stock-manage.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-stock-manage',
+  templateUrl: './stock-manage.component.html',
+  styleUrls: ['./stock-manage.component.css']
+})
+export class StockManageComponent implements OnInit {
+
+  public stocks: Array<Stock>;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.stocks = [
+      new Stock(1, '第一只股票', 1.99, 3.5, '这是一支股票, 是我在学习慕课网在实战是创建的。', ['IT', '互联网']),
+      new Stock(2, '第二只股票', 2.99, 2.5, '这是二支股票, 是我在学习慕课网在实战是创建的。', ['IT', '互联网']),
+      new Stock(3, '第三只股票', 3.99, 1.5, '这是三支股票, 是我在学习慕课网在实战是创建的。', ['IT', '互联网']),
+      new Stock(4, '第四只股票', 4.99, 3.0, '这是四支股票, 是我在学习慕课网在实战是创建的。', ['IT', '互联网']),
+      new Stock(5, '第五只股票', 5.99, 4.5, '这是五支股票, 是我在学习慕课网在实战是创建的。', ['IT', '互联网']),
+      new Stock(6, '第六只股票', 6.99, 5.0, '这是六支股票, 是我在学习慕课网在实战是创建的。', ['IT', '互联网'])
+    ];
+  }
+
+}
+
+export class Stock {
+  constructor(public id: number,
+              public name: string,
+              public price: number,
+              public rating: number,
+              public desc: string,
+              public categories: Array<string>) {
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
